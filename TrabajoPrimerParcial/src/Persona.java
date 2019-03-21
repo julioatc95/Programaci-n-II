fd1d68e017916b97a5510cb5dd29413e207d0918
julioatc95 <40080402+julioatc95@users.noreply.github.com>
2019-03-20 22:07:33 -0600

    Add files via upload

---
+++ TrabajoPrimerParcial/src/Persona.java
@@ -0,0 +1,116 @@
+public class Persona {
+    
+    
+    
+    String Nombre, Depa, Cod, Suel, Com, Bon, Igg, Isr, sl;
+   
+
+    public Persona(String Nombre, String Depa, String Cod, String Suel, String Com, String Bon, String Igg, String Isr, String sl) {
+       
+        
+        this.Nombre = Nombre;
+        this.Depa = Depa;
+        this.Cod = Cod;
+        this.Suel = Suel;
+        this.Com = Com;
+        this.Bon = Bon;
+        this.Igg = Igg;
+        this.Isr = Isr;
+        this.sl = sl;
+     
+    }
+
+    
+
+   
+
+    public String getSl() {
+        return sl;
+    }
+
+    public void setSl(String sl) {
+        this.sl = sl;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getDepa() {
+        return Depa;
+    }
+
+    public void setDepa(String Depa) {
+        this.Depa = Depa;
+    }
+
+    public String getCod() {
+        return Cod;
+    }
+
+    public void setCod(String Cod) {
+        this.Cod = Cod;
+    }
+
+    public String getSuel() {
+        return Suel;
+    }
+
+    public void setSuel(String Suel) {
+        this.Suel = Suel;
+    }
+
+    public String getCom() {
+        return Com;
+    }
+
+    public void setCom(String Com) {
+        this.Com = Com;
+    }
+
+    public String getBon() {
+        return Bon;
+    }
+
+    public void setBon(String Bon) {
+        this.Bon = Bon;
+    }
+
+    public String getIgg() {
+        return Igg;
+    }
+
+    public void setIgg(String Igg) {
+        this.Igg = Igg;
+    }
+
+    public String getIsr() {
+        return Isr;
+    }
+
+    public void setIsr(String Isr) {
+        this.Isr = Isr;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
